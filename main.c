565285d40fdd9cff5401120e4bf9d993137e0a2a
Leonardotei <leonardobruce1@gmail.com>
2022-08-27 19:03:54 -0300

    add limitador da nota que se pode inserir

--- main.c
+++ main.c
@@ -11,14 +11,38 @@ int main()
     float mediaAnual;
 
     // Entrada
+    // Nota 1
     printf("Digite nota 1: ");
     scanf("%f", &nota1);
+    if (nota1 >10 || nota1 <0)
+    {
+        printf("Nota invalida");
+        abort();
+    }
+    // Nota 2
     printf("Digite nota 2: ");
     scanf("%f", &nota2);
+    if (nota2 >10 || nota2 <0)
+    {
+        printf("Nota invalida");
+        abort();
+    }
+    // Nota 3
     printf("Digite nota 3: ");
     scanf("%f", &nota3);
+    if (nota3 >10 || nota3 <0)
+    {
+        printf("Nota invalida");
+        abort();
+    }
+    // Nota 4
     printf("Digite nota 4: ");
     scanf("%f", &nota4);
+    if (nota4 >10 || nota4 <0)
+    {
+        printf("Nota invalida");
+        abort();
+    }
     // Processamento
     mediaAnual = (nota1 + nota2 + nota3 + nota4) / 4;
 
