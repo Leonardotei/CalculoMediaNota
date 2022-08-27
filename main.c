e5ce153c2c8d5267801bccfee789d483b9879db8
Leonardotei <leonardobruce1@gmail.com>
2022-08-27 13:43:51 -0300

    add 4 variáveis

--- main.c
+++ main.c
@@ -6,6 +6,8 @@ int main()
     // Declaração de variáveis
     float nota1;
     float nota2;
+    float nota3;
+    float nota4;
     float mediaAnual;
 
     // Entrada
@@ -13,9 +15,12 @@ int main()
     scanf("%f", &nota1);
     printf("Digite nota 2: ");
     scanf("%f", &nota2);
-
+    printf("Digite nota 3: ");
+    scanf("%f", &nota3);
+    printf("Digite nota 4: ");
+    scanf("%f", &nota4);
     // Processamento
-    mediaAnual = (nota1 + nota2) / 2;
+    mediaAnual = (nota1 + nota2 + nota3 + nota4) / 4;
 
     // Criticar para saber se o aluno esta aprovado ou retido
     // Saída
