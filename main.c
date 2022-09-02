97735e33b535fff1b4c526e1f0550d3db46b0ae1
Leonardotei <leonardobruce1@gmail.com>
2022-09-02 17:07:41 -0300

    add otimização do código

--- main.c
+++ main.c
@@ -3,46 +3,42 @@
 
 int main()
 {
-    // Declara��o de vari�veis
-    float nota1;
-    float nota2;
-    float nota3;
-    float nota4;
-    float exame;
-    float mediaAnual;
-    float mediaFinal;
+    // Declaracao de variaveis
+    int aluno;
+    float nota1, nota2, nota3, nota4, exame, mediaAnual, mediaFinal;
 
     // Entrada
+    printf("\tCalculo de media do aluno\n");
     // Nota 1
-    printf("Digite nota 1: ");
+    printf("\nDigite nota 1: ");
     scanf("%f", &nota1);
     if (nota1 >10 || nota1 <0)
     {
-        printf("Nota invalida");
+        printf("\nNota invalida");
         abort();
     }
     // Nota 2
-    printf("Digite nota 2: ");
+    printf("\nDigite nota 2: ");
     scanf("%f", &nota2);
     if (nota2 >10 || nota2 <0)
     {
-        printf("Nota invalida");
+        printf("\nNota invalida");
         abort();
     }
     // Nota 3
-    printf("Digite nota 3: ");
+    printf("\nDigite nota 3: ");
     scanf("%f", &nota3);
     if (nota3 >10 || nota3 <0)
     {
-        printf("Nota invalida");
+        printf("\nNota invalida");
         abort();
     }
     // Nota 4
-    printf("Digite nota 4: ");
+    printf("\nDigite nota 4: ");
     scanf("%f", &nota4);
     if (nota4 >10 || nota4 <0)
     {
-        printf("Nota invalida");
+        printf("\nNota invalida");
         abort();
     }
     // Processamento
@@ -52,15 +48,15 @@ int main()
     // Sa�da
     if (mediaAnual >= 7)
     {
-        printf("APROVADO!!");
+        printf("\nAPROVADO!!");
     }
     else
     {
-        printf("Reprovado, aluno devera realizar exame. Insira nota do exame: ");
+        printf("\nReprovado, aluno devera realizar exame. Insira nota do exame: ");
         scanf("%f", &exame);
         if (exame>10 || exame<0)
         {
-            printf("NOTA INVALIDA!");
+            printf("\nNOTA INVALIDA!");
             abort();
         }
 
@@ -68,11 +64,11 @@ int main()
 
         if(mediaFinal>=5)
         {
-            printf("APROVADO!!");
+            printf("\nAPROVADO!!");
         }
         else
         {
-            printf("REPROVADO!!");
+            printf("\nREPROVADO!!");
         }
     }
     return 0;
